62ab7bc413070c76e0a72cafb69455fa93028f82
Joseph Wambua <josefwambua2@gmail.com>
2024-07-31 11:58:05 +0300

    handleclickevent

--- larareact/firstapp/src/App.js
+++ larareact/firstapp/src/App.js
@@ -5,26 +5,33 @@ import axios from "axios";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
     // state
     this.state = {
       users: [],
-
     };
   }
   getUsers() {
     this.setState({
-      loading: true}
-    )
+      loading: true,
+    });
     axios("https://api.randomuser.me/?nat=US&results=5").then((response) =>
       // rendering changes using useState/ update changes
       // provides instant changes when user makes changes to the state
       this.setState({
-        users: response.data.results,
-        loading: false
+        // merge users  to load more data
+        users: [...this.state.users, ...response.data.results],
+        loading: false,
       })
     );
   }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.getUsers();
+    console.log("users loaded");
+  }
+
   componentWillMount() {
     this.getUsers();
   }
@@ -50,89 +57,86 @@ class App extends Component {
         </h1>
         {/* check if the state is loading and display users otherwise display loading*/}
         <div className="user-list">
-          {!this.state.loading
-            ? this.state.users.map((user) => (
-                <div
-                  key={user.id}
-                  className="user-card"
+          {!this.state.loading ? (
+            this.state.users.map((user) => (
+              <div
+                key={user.id}
+                className="user-card"
+                style={{
+                  marginBottom: "20px",
+                  padding: "15px",
+                  border: "1px solid #ddd",
+                  borderRadius: "5px",
+                }}
+              >
+                {/* <h2 style={{color: '#444', marginBottom: '10px'}}>{user.name.first} {user.name.last}</h2> */}
+                {/* <p><strong>Email:</strong> <a href={`mailto:${user.email}`} style={{color: '#0066cc'}}>{user.email}</a></p> */}
+                {/* manipulating to a table */}
+                <table
+                  className="user-details"
                   style={{
-                    marginBottom: "20px",
-                    padding: "15px",
-                    border: "1px solid #ddd",
-                    borderRadius: "5px",
+                    width: "100%",
+                    borderCollapse: "collapse",
+                    marginTop: "15px",
                   }}
                 >
-                  {/* <h2 style={{color: '#444', marginBottom: '10px'}}>{user.name.first} {user.name.last}</h2> */}
-                  {/* <p><strong>Email:</strong> <a href={`mailto:${user.email}`} style={{color: '#0066cc'}}>{user.email}</a></p> */}
-                  {/* manipulating to a table */}
-                  <table
-                    className="user-details"
-                    style={{
-                      width: "100%",
-                      borderCollapse: "collapse",
-                      marginTop: "15px",
-                    }}
-                  >
-                    <thead>
-                      <tr style={{ backgroundColor: "#f2f2f2" }}>
-                        <th
-                          style={{
-                            border: "1px solid #ddd",
-                            padding: "10px",
-                            textAlign: "left",
-                          }}
-                        >
-                          First Name
-                        </th>
-                        <th
-                          style={{
-                            border: "1px solid #ddd",
-                            padding: "10px",
-                            textAlign: "left",
-                          }}
-                        >
-                          Last Name
-                        </th>
-                        <th
-                          style={{
-                            border: "1px solid #ddd",
-                            padding: "10px",
-                            textAlign: "left",
-                          }}
-                        >
-                          Email
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td
-                          style={{ border: "1px solid #ddd", padding: "10px" }}
-                        >
-                          {user.name.first}
-                        </td>
-                        <td
-                          style={{ border: "1px solid #ddd", padding: "10px" }}
-                        >
-                          {user.name.last}
-                        </td>
-                        <td
-                          style={{ border: "1px solid #ddd", padding: "10px" }}
-                        >
-                          {user.email}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              ))
-            : <Loading message="woooow" />
-            }
+                  <thead>
+                    <tr style={{ backgroundColor: "#f2f2f2" }}>
+                      <th
+                        style={{
+                          border: "1px solid #ddd",
+                          padding: "10px",
+                          textAlign: "left",
+                        }}
+                      >
+                        First Name
+                      </th>
+                      <th
+                        style={{
+                          border: "1px solid #ddd",
+                          padding: "10px",
+                          textAlign: "left",
+                        }}
+                      >
+                        Last Name
+                      </th>
+                      <th
+                        style={{
+                          border: "1px solid #ddd",
+                          padding: "10px",
+                          textAlign: "left",
+                        }}
+                      >
+                        Email
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td style={{ border: "1px solid #ddd", padding: "10px" }}>
+                        {user.name.first}
+                      </td>
+                      <td style={{ border: "1px solid #ddd", padding: "10px" }}>
+                        {user.name.last}
+                      </td>
+                      <td style={{ border: "1px solid #ddd", padding: "10px" }}>
+                        {user.email}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            ))
+          ) : (
+            <Loading message="woooow" />
+          )}
         </div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="submit" value=" load users" className="styled" />
+        </form>
       </div>
     );
   }
 }
 
 export default App;
- 
