bb34bf79a985b3d56b6dfaf234a944ea02a3a98c
Joseph Wambua <josefwambua2@gmail.com>
2024-08-05 13:28:55 +0300

    useeffect

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -4,29 +4,34 @@ const App = () => {
   // state
   const [news, setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState('kenya-politics');
-
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=')
 
   const handleChange = (e) => {
     setSearchQuery(e.target.value);
   }
+  useEffect(()=> {
   // fetch news
   const fetchNews = () => {
-    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    fetch(url)
       .then(result => result.json())
       .then(data => setNews(data.hits))
       .catch(err => console.error(err))
   };
-  useEffect(()=> {
-    fetchNews ()
-  },
-  [searchQuery]);
+  
+    fetchNews ();
+  }, [url]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+  }
 
   return (
     <div>
       <h2>
         News
       </h2>
-      <form>
+      <form onSubmit={handleSubmit}>
       <input type="text" value={searchQuery} onChange={handleChange}/>        <button>Search</button>
       </form>
       {news.map((n,i) => (<p key={i}>{n.title}</p>))}
