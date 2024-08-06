4ad6219f4bc7a6af67dce3ba4d05828e9c4ac1c1
Joseph Wambua <josefwambua2@gmail.com>
2024-08-06 12:05:13 +0300

    fullnwpp

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -3,38 +3,50 @@ import React, {useState, useEffect } from "react";
 const App = () => {
   // state
   const [news, setNews] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('kenya-politics');
+  const [searchQuery, setSearchQuery] = useState('');
   const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=')
 
   const handleChange = (e) => {
     setSearchQuery(e.target.value);
   }
-  useEffect(()=> {
+// loader
+const [loading, setLoading] = useState(false);
+
   // fetch news
+  useEffect(()=> {
+
   const fetchNews = () => {
     fetch(url)
       .then(result => result.json())
-      .then(data => setNews(data.hits))
+      .then(data => setNews(data.hits), setLoading(false))
+
       .catch(err => console.error(err))
   };
-  
+  setLoading(true);
     fetchNews ();
   }, [url]);
 
   const handleSubmit = e => {
     e.preventDefault();
-    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
-  }
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  };
+  const showLoading = () => (loading ? <h2>Loading...</h2> : "");
 
+  const searchForm = () => (
+  <form onSubmit={handleSubmit}>
+  <input type="text" value={searchQuery} onChange={handleChange}/>        <button>Search</button>
+  </form>
+  )
+
+  const showNews = () => news.map((n,i) => (<p key={i}>{n.title}</p>));
   return (
     <div>
       <h2>
         News
       </h2>
-      <form onSubmit={handleSubmit}>
-      <input type="text" value={searchQuery} onChange={handleChange}/>        <button>Search</button>
-      </form>
-      {news.map((n,i) => (<p key={i}>{n.title}</p>))}
+     {showLoading()}
+     {searchForm()}
+      {showNews()}
     </div>
   )
 };
